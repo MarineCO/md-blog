42655260f7a933ae2470940f852534509e880a58
marineco <marinecolonge@gmail.com>
2016-10-25 10:32:37 +0200

    mise en place ajax + récupération post Alice en markdown à convertir en html

--- js/app.js
+++ js/app.js
@@ -2,7 +2,31 @@
 	"use strict";
 	var app = {
 		init:function(){
-			// let's go
+			//this.listeners();
+			this.getData();
+		},
+
+		listeners: function() {
+			//$('#btn').on('click', this.getData.bind(this));
+		},
+
+		getData: function() {
+			$.ajax("http://192.168.1.21:1337/alice.md")
+			.done(this.ajaxDone)
+			.fail(this.ajaxFail)
+			.always(this.ajaxAlways);
+		},
+
+		ajaxDone: function(response) {
+			console.log(response);
+		},
+
+		ajaxFail: function() {
+			console.log('erreur');
+		},
+
+		ajaxAlways: function() {
+			console.log('complete');
 		},
 	};
 
