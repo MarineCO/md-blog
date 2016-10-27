c1ce883d68cf3bca239f68d7304ec440497a9c30
marineco <marinecolonge@gmail.com>
2016-10-27 11:34:10 +0200

    refactorisation check : 1 requête ajax pr récup les 2 articles et 1 seul listener

--- js/app.js
+++ js/app.js
@@ -1,97 +1,59 @@
 (function(){
 	"use strict";
 	var app = {
+
+		url: "http://192.168.1.40:1337/",
+		json: "menu.json",
+		getPath: null,
+
 		init:function(){
 			this.getDataMenu();
+			var self = this;
+			self.listeners();
 		},
 
 		listeners: function() {
-			$(".new0").on('click', this.getDataExample.bind(this));
-			$(".new1").on('click', this.getDataAlice.bind(this));
+			$("#menu").on('click', 'button', this.clickBtn);
 		},
 
 		getDataMenu: function() {
-			$.ajax("http://192.168.1.40:1337/menu.json")
+			$.ajax(this.url + this.json)
 			.done(this.menuDone)
-			.fail(this.menuFail)
-			.always(this.menuAlways);
-		},
-
-		getDataAlice: function() {
-			$.ajax("http://192.168.1.40:1337/alice.md")
-			.done(this.aliceDone)
-			.fail(this.aliceFail)
-			.always(this.aliceAlways);
-		},
-
-		getDataExample: function() {
-			$.ajax("http://192.168.1.40:1337/example.md")
-			.done(this.exampleDone)
-			.fail(this.exampleFail)
-			.always(this.exampleAlways);
+			.fail(this.fail);
 		},
 
 		menuDone: function(response) {
-			console.log(response.menu);
-
 			for (var i = 0; i < response.menu.length; i++) {
-				
-				var obj = response.menu[i].title;
-
-				$('#menu').append('<li>' + '<button>' + obj + '</button>' + '</li>');
-				$('#menu li button').addClass(function(index) {
-					return "new" + index;
-				});
-				app.listeners();
+				var path = response.menu[i].path;
+				var title = response.menu[i].title;
+				$('#menu').append('<li>' + '<button data-path="'+path+'">' + title + '</button>' + '</li>');
 			}
 		},
 
-		menuFail: function() {
-			console.log('erreur menu');
+		clickBtn: function() {
+			this.getPath = $(this).data("path");
+			$.ajax(app.url + this.getPath)
+			.done(app.clickBtnDone)
+			.fail(app.failClick);
 		},
 
-		menuAlways: function() {
-			console.log('complete menu');
-		},
-		
-		aliceDone: function(response) {
+		clickBtnDone: function(response) {
 			var converter = new showdown.Converter(),
 			text = response,
 			html = converter.makeHtml(text);
-
-			$('#md').html(html);
+			$('#app').html(html);
 		},
 
-		aliceFail: function() {
-			console.log('erreur alice');
+		fail: function() {
+			console.log('erreur');
 		},
 
-		aliceAlways: function() {
-			console.log('complete alice');
-		},
-
-		exampleDone: function(response) {
-			var converter = new showdown.Converter(),
-			text = response,
-			html = converter.makeHtml(text);
-
-			$('#md').html(html);
-		},
-
-		exampleFail: function() {
-			console.log('erreur example');
-		},
-
-		exampleAlways: function() {
-			console.log('complete example');
-		},
-
+		failClick: function() {
+			console.log('erreur click');
+		}
 	};
-
 
 	$(document).ready(function(){
 		app.init();
 	});
 })();
-
-//pr refactorisation avoir une seule requête ajax / 1 adresse http qui change en fonction de la requête demandée
